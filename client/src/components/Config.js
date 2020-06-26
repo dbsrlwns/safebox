04f80c9a258f0383dcb67c241479729ecc52b19b
sdc02408 <sdc02408@naver.com>
2020-06-26 20:03:24 +0900

    폴리라인

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,4 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+export const NaverMapAPI_KEY ='https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=1pq9uu5p8s&callback=initMap';
+export const REACT_APP_NOT_SECRET_COD ='https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=1pq9uu5p8s&callback=initMap';
